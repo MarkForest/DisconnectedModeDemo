db9975456bf43df542fd4c0436cfefd51fce7557
markforest <toxerius7@gmail.com>
2018-09-05 15:35:01 +0300

    commit 1

---
+++ DataTableTest/DataSetDemo/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace DataSetDemo
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn = null;
+        private SqlDataAdapter da = null;
+        private DataSet set = null;
+        private SqlCommandBuilder cmd = null;
+        string cs = "";
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection();
+            cs = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            conn.ConnectionString = cs;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(cs);
+                set = new DataSet();
+                string sql = textBox1.Text;
+                da = new SqlDataAdapter(sql, conn);
+                dataGridView1.DataSource = null;
+                cmd = new SqlCommandBuilder(da);
+                da.Fill(set);
+                dataGridView1.DataSource = set.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            da.Update(set);
+        }
+    }
+}
---
+++ DataTableTest/DataTableTest/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace DataTableTest
+{
+    public partial class Form1 : Form
+    {
+        private SqlDataReader reader;
+        private DataTable table;
+        private SqlConnection conn;
+        private string cs = "";
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection();
+            cs = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            conn.ConnectionString = cs;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.CommandText = textBox1.Text;
+                comm.Connection = conn;
+                dataGridView1.DataSource = null;
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    MessageBox.Show("Соединение подключенно успешно!");
+                }
+                table = new DataTable();
+                reader = comm.ExecuteReader();
+                int line = 0;
+                while (reader.Read())
+                {
+                    //создание полей
+                    if(line == 0)
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            table.Columns.Add(reader.GetName(i));
+                        }
+                        line++;
+                    }
+
+                    //наполняем ряды
+                    DataRow row = table.NewRow();
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        row[i] = reader[i];
+                    }
+                    table.Rows.Add(row);
+                }
+                dataGridView1.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn?.Close();
+                reader?.Close();
+            }
+        }
+    }
+}
